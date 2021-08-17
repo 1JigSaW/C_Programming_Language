a7e88f14a3eb04bc1b8a369a70d1629b983fd4c0
jigsaw <misho_1998@mail.ru>
2021-08-17 23:49:15 +0300

    1.14

---
+++ A_Tutorial_Introduction/1_14_gist_freq.c
@@ -0,0 +1,36 @@
+/*
+ Упражнение 1.14. Напишите программу, печатающую гистограммы частот встречаемости вводимых
+ символов.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int c = 0;
+	int arr[127];
+	for (int i = 0; i < 128; i++)
+		arr[i] = '0';
+	while ((c = getchar()) != EOF)
+    	{
+        	if (c < 128)    
+            		++arr[c]; 
+    	}
+     
+    	for (int i = 32; i < 127; ++i)
+    	{
+		if (arr[i] > 0) {
+            		printf("\"");
+            		putchar(i); 
+            		printf("\": ");
+            		for (int j = 1; j <= arr[i]; ++j)
+                		printf("#");
+            		printf("\n");
+        	}
+    	}
+
+    	return 0;
+}
+
+
+
